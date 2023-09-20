caa25e0ebbd1467f2a6f52ad9bb41c41ee7646fa
Daiana <alonsodaiana5@gmail.com>
2023-09-20 19:44:46 -0300

    cambios

---
+++ src/main/java/ar/unrn/tp/dto/PromocionDTO.java
@@ -0,0 +1,16 @@
+package ar.unrn.tp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class PromocionDTO {
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Double porcentaje;
+    private String tipo;
+    private String descripcion;
+}
